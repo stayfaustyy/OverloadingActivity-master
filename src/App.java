0a0d36f1ec79c5948852b2e2abc2a704a161f614
stayfaustyy <yvesagustin12@yahoo.com>
2022-04-14 23:32:56 +0800

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,45 @@
+public class App {
+
+    private static int sum2(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private static int sum3(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    /**
+     * Modify the methods in such a way that the Class App
+     * can now have 2 overloaded computeSum() methods.
+     * One computing the sum of two numbers, and one computing the sum
+     * of three numbers.
+     */
+
+    /**
+     * 
+     * Create method that returns the greater integer value
+     */
+
+    /**
+     * 
+     * Create method that returns the greater double value
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /**
+         * Create Method calls that takes in 2 parameters, and 3 parameters
+         * respectively and computes their sum.
+         */
+
+        /**
+         * Call the greaterValue method with integer parameters
+         */
+        System.out.println("The greater value between 45 and 100 is " + greaterValue(45, 100));
+
+        /**
+         * Call the greaterValue method with double parameters
+         */
+        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14, 9.8));
+
+    }
+}
